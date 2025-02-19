807af761fc44a5f21d22fa96f71cc3edd94a78bd
Danilukkk <130748063+Danilukkk@users.noreply.github.com>
2025-02-20 00:34:43 +0300

    as

---
+++ src/test/java/org/example/l13_3/ApiTest.java
@@ -0,0 +1,32 @@
+package org.example.l13_3;
+
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.baseURI;
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class ApiTest {
+    private final int nullPet = 1;
+
+
+    @BeforeEach
+    public void setUp() {
+        RestAssured.baseURI = "https://petstore.swagger.io/v2";
+    }
+
+    @Test
+    public void search() {
+        given().when()
+                .get(baseURI + "/pet/{petId}", nullPet)
+                .then()
+                .statusCode(404)
+                .statusLine("HTTP/1.1 404 Not Found")
+                .body("type", equalTo("error"))
+                .log().all()
+                .body("message", equalTo("Pet not found"));
+    }
+
+}
