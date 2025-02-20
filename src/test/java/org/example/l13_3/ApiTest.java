e83b12e5539d40268691bf1d9e7273eaa40d19e5
Danilukkk <130748063+Danilukkk@users.noreply.github.com>
2025-02-20 22:22:58 +0300

    add post

--- src/test/java/org/example/l13_3/ApiTest.java
+++ src/test/java/org/example/l13_3/ApiTest.java
@@ -2,7 +2,11 @@ package org.example.l13_3;
 
 import io.restassured.RestAssured;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import static io.restassured.RestAssured.baseURI;
 import static io.restassured.RestAssured.given;
@@ -18,6 +22,7 @@ public class ApiTest {
     }
 
     @Test
+    @DisplayName("Get запрос отвечает ошибкой 404 при отсутствии питомца")
     public void search() {
         given().when()
                 .get(baseURI + "/pet/{petId}", nullPet)
@@ -29,4 +34,36 @@ public class ApiTest {
                 .body("message", equalTo("Pet not found"));
     }
 
+    @Test
+    @DisplayName("Создаем нового пользователя")
+    public void addNewUser() {
+        Integer id = 78094;
+        String username = "Danilukkk";
+        String firstName = "Daniil";
+        String lastName = "Fedyashov";
+        String email = "df@m.com";
+        String password = "asdfgh";
+        String phone = "123456789";
+        Integer userStatus = 1;
+
+        Map<String, String> request = new HashMap<>();
+        request.put("id", id.toString());
+        request.put("username", username);
+        request.put("firstName", firstName);
+        request.put("lastName", lastName);
+        request.put("email", email);
+        request.put("password", password);
+        request.put("phone", phone);
+        request.put("userStatus", userStatus.toString());
+
+        given().contentType("application/json")
+                .body(request)
+                .when()
+                .post(baseURI + "/user")
+                .then()
+                .log().all()
+                .assertThat()
+                .statusCode(200)
+                .body("message", equalTo(id.toString()));
+    }
 }
